f333ad44a32f6e6fe0462a9d0fba367a40ea7818
hoang anh <hoanganhlhbk@gmail.com>
2023-12-02 15:09:03 +0700

    rewrite redis connection config

--- helpers/redis.js
+++ helpers/redis.js
@@ -1,5 +1,7 @@
 const { createClient } = require('redis');
-const client = createClient();
+const client = createClient({
+    url: `redis://${process.env.REDIS_USER}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+});
 
 client.on('error', err => console.log('Redis Client Error', err));
 client.connect();

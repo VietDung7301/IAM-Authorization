060be8b011e590a45c0cc55a8f6b359761352a6f
Hoang Anh <hoanganhlhbk@gmail.com>
2024-05-28 09:28:20 +0700

    debug commit

--- back_end/access/middleware/ipgeo/ipgeo.go
+++ back_end/access/middleware/ipgeo/ipgeo.go
@@ -107,7 +107,7 @@ func (igmw *IpGeoMiddleware) Handler(next http.Handler) http.Handler {
 		// 	responses.Response(w, http.StatusBadRequest, ips[0], nil)
 		// 	return
 		// }
-		locationCheckV2(ips[0])
+		fmt.Print(locationCheckV2(ips[0]))
 		next.ServeHTTP(w, r)
 		// return
 	})
@@ -125,34 +125,20 @@ func locationCheckV2(ipAddress string) bool {
 		fmt.Print(err)
 		return false
 	}
+	db.Close()
+
 	fmt.Printf("country_short: %s\n", results.Country_short)
 	fmt.Printf("country_long: %s\n", results.Country_long)
 	fmt.Printf("region: %s\n", results.Region)
 	fmt.Printf("city: %s\n", results.City)
-	fmt.Printf("isp: %s\n", results.Isp)
 	fmt.Printf("latitude: %f\n", results.Latitude)
 	fmt.Printf("longitude: %f\n", results.Longitude)
-	fmt.Printf("domain: %s\n", results.Domain)
 	fmt.Printf("zipcode: %s\n", results.Zipcode)
 	fmt.Printf("timezone: %s\n", results.Timezone)
-	fmt.Printf("netspeed: %s\n", results.Netspeed)
-	fmt.Printf("iddcode: %s\n", results.Iddcode)
-	fmt.Printf("areacode: %s\n", results.Areacode)
-	fmt.Printf("weatherstationcode: %s\n", results.Weatherstationcode)
-	fmt.Printf("weatherstationname: %s\n", results.Weatherstationname)
-	fmt.Printf("mcc: %s\n", results.Mcc)
-	fmt.Printf("mnc: %s\n", results.Mnc)
-	fmt.Printf("mobilebrand: %s\n", results.Mobilebrand)
-	fmt.Printf("elevation: %f\n", results.Elevation)
-	fmt.Printf("usagetype: %s\n", results.Usagetype)
-	fmt.Printf("addresstype: %s\n", results.Addresstype)
-	fmt.Printf("category: %s\n", results.Category)
-	fmt.Printf("district: %s\n", results.District)
-	fmt.Printf("asn: %s\n", results.Asn)
-	fmt.Printf("as: %s\n", results.As)
-	fmt.Printf("api version: %s\n", ip2location.Api_version())
 
-	db.Close()
+	if results.Country_short != "VN" || results.Country_long != "Viet Nam" {
+		return false
+	}
 
 	return true
 }

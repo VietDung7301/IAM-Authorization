59e43ab72f1a3165530cddc23535b11d2634a341
Hoang Anh <hoanganhlhbk@gmail.com>
2024-05-28 09:38:20 +0700

    finish update ipgeo mdw

--- back_end/access/middleware/ipgeo/ipgeo.go
+++ back_end/access/middleware/ipgeo/ipgeo.go
@@ -1,22 +1,18 @@
 package ipgeo
 
 import (
-	"access/helpers/jsonparse"
-	// "access/helpers/jwtparse"
-	// "access/helpers/responses"
+	"access/helpers/jwtparse"
+	"access/helpers/responses"
 
-	// "context"
-	// "encoding/json"
+	"context"
+	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
-	"os"
 	"strings"
 
-	// "time"
+	"time"
 
 	"github.com/ip2location/ip2location-go/v9"
-	"github.com/joho/godotenv"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -33,83 +29,79 @@ type MarkedUserData struct {
 func (igmw *IpGeoMiddleware) Handler(next http.Handler) http.Handler {
 	fmt.Printf("ip geo mdw in\n")
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// authorization := r.Header.Get("Authorization")
-		// if authorization == "" {
-		// 	responses.ResponseInvalidRequest(w)
-		// 	return
-		// }
-		// arr := strings.Split(authorization, " ")
-		// token := arr[1]
-		// claims := jwtparse.GetClaims(token)
-		// if claims == nil {
-		// 	responses.ResponseInvalidRequest(w)
-		// 	return
-		// }
+		authorization := r.Header.Get("Authorization")
+		if authorization == "" {
+			responses.ResponseInvalidRequest(w)
+			return
+		}
+		arr := strings.Split(authorization, " ")
+		token := arr[1]
+		claims := jwtparse.GetClaims(token)
+		if claims == nil {
+			responses.ResponseInvalidRequest(w)
+			return
+		}
 
 		ips := strings.Split(r.Header.Get("X-FORWARDED-FOR"), ", ")
 		fmt.Printf("req ip addr: %s\n", ips[0])
 
-		// if locationCheck(ips[0]) {
-		// 	fmt.Printf("ip geo mdw out\n")
-		// 	next.ServeHTTP(w, r)
-		// } else {
-		// 	var markedUserData MarkedUserData
-		// 	ctx := context.Background()
-		// 	redisKey := fmt.Sprintf("marked_user@%s", claims["sub"].(string))
+		if locationCheckV2(ips[0]) {
+			fmt.Printf("ip geo mdw out\n")
+			next.ServeHTTP(w, r)
+		} else {
+			var markedUserData MarkedUserData
+			ctx := context.Background()
+			redisKey := fmt.Sprintf("marked_user@%s", claims["sub"].(string))
 
-		// 	if igmw.RedisClient.Exists(ctx, redisKey).Val() != 0 {
-		// 		val, err := igmw.RedisClient.Get(ctx, redisKey).Result()
-		// 		if err != nil {
-		// 			fmt.Printf("Ko lay duoc marked user\n")
-		// 			responses.ResponseGeneralError(w, "internal server err")
-		// 			return
-		// 		}
-		// 		json.Unmarshal([]byte(val), &markedUserData)
-		// 		// nên set valid time ở env | default = 1 day
-		// 		if markedUserData.Is_checked == 1 && (markedUserData.Checked_at+86400) > time.Now().Unix() {
-		// 			fmt.Printf("ip geo mdw out\n")
-		// 			next.ServeHTTP(w, r)
-		// 			return
-		// 		}
-		// 	}
+			if igmw.RedisClient.Exists(ctx, redisKey).Val() != 0 {
+				val, err := igmw.RedisClient.Get(ctx, redisKey).Result()
+				if err != nil {
+					fmt.Printf("Ko lay duoc marked user\n")
+					responses.ResponseGeneralError(w, "internal server err")
+					return
+				}
+				json.Unmarshal([]byte(val), &markedUserData)
+				// nên set valid time ở env | default = 1 day
+				if markedUserData.Is_checked == 1 && (markedUserData.Checked_at+86400) > time.Now().Unix() {
+					fmt.Printf("ip geo mdw out\n")
+					next.ServeHTTP(w, r)
+					return
+				}
+			}
 
-		// 	// mark user
-		// 	markedUserData = MarkedUserData{
-		// 		Is_checked:  0,
-		// 		Checked_at:  0,
-		// 		Last_2FA_at: "",
-		// 	}
-		// 	// save to redis
-		// 	payload, err := json.Marshal(markedUserData)
-		// 	if err != nil {
-		// 		fmt.Printf("%s\n", err.Error())
-		// 	}
+			// mark user
+			markedUserData = MarkedUserData{
+				Is_checked:  0,
+				Checked_at:  0,
+				Last_2FA_at: "",
+			}
+			// save to redis
+			payload, err := json.Marshal(markedUserData)
+			if err != nil {
+				fmt.Printf("%s\n", err.Error())
+			}
 
-		// 	err = igmw.RedisClient.Set(ctx, redisKey, payload, 0).Err()
-		// 	if err != nil {
-		// 		fmt.Printf("%s\n", err.Error())
-		// 	}
+			err = igmw.RedisClient.Set(ctx, redisKey, payload, 0).Err()
+			if err != nil {
+				fmt.Printf("%s\n", err.Error())
+			}
 
-		// 	// delete public key
-		// 	redisKey = fmt.Sprintf("%s@%sAccessToken", claims["client_id"].(string), claims["sub"].(string))
-		// 	err = igmw.RedisClient.Del(ctx, redisKey).Err()
-		// 	if err != nil {
-		// 		fmt.Printf("%s\n", err.Error())
-		// 	}
+			// delete public key
+			redisKey = fmt.Sprintf("%s@%sAccessToken", claims["client_id"].(string), claims["sub"].(string))
+			err = igmw.RedisClient.Del(ctx, redisKey).Err()
+			if err != nil {
+				fmt.Printf("%s\n", err.Error())
+			}
 
-		// 	redisKey = fmt.Sprintf("%s@%sRefreshToken", claims["client_id"].(string), claims["sub"].(string))
-		// 	err = igmw.RedisClient.Del(ctx, redisKey).Err()
-		// 	if err != nil {
-		// 		fmt.Printf("%s\n", err.Error())
-		// 	}
+			redisKey = fmt.Sprintf("%s@%sRefreshToken", claims["client_id"].(string), claims["sub"].(string))
+			err = igmw.RedisClient.Del(ctx, redisKey).Err()
+			if err != nil {
+				fmt.Printf("%s\n", err.Error())
+			}
 
-		// 	// responses.Response(w, http.StatusBadRequest, "location invalid!", nil)
-		// 	responses.Response(w, http.StatusBadRequest, ips[0], nil)
-		// 	return
-		// }
-		fmt.Printf("location in VN: %t\n", locationCheckV2(ips[0]))
-		next.ServeHTTP(w, r)
-		// return
+			responses.Response(w, http.StatusBadRequest, "location invalid!", nil)
+			return
+		}
 	})
 }
 
@@ -127,74 +119,18 @@ func locationCheckV2(ipAddress string) bool {
 	}
 	db.Close()
 
-	fmt.Printf("country_short: %s\n", results.Country_short)
-	fmt.Printf("country_long: %s\n", results.Country_long)
-	fmt.Printf("region: %s\n", results.Region)
-	fmt.Printf("city: %s\n", results.City)
-	fmt.Printf("latitude: %f\n", results.Latitude)
-	fmt.Printf("longitude: %f\n", results.Longitude)
-	fmt.Printf("zipcode: %s\n", results.Zipcode)
-	fmt.Printf("timezone: %s\n", results.Timezone)
+	// fmt.Printf("country_short: %s\n", results.Country_short)
+	// fmt.Printf("country_long: %s\n", results.Country_long)
+	// fmt.Printf("region: %s\n", results.Region)
+	// fmt.Printf("city: %s\n", results.City)
+	// fmt.Printf("latitude: %f\n", results.Latitude)
+	// fmt.Printf("longitude: %f\n", results.Longitude)
+	// fmt.Printf("zipcode: %s\n", results.Zipcode)
+	// fmt.Printf("timezone: %s\n", results.Timezone)
 
 	if results.Country_short != "VN" || results.Country_long != "Viet Nam" {
 		return false
 	}
 
 	return true
-}
-
-func locationCheck(ipAddress string) bool {
-	// make api call
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Printf("Error loading .env file - ipgeo\n")
-	}
-	client := &http.Client{}
-	apiKey := os.Getenv("IP_GEO_API_KEY")
-	apiUrl := os.Getenv("IP_GEO_URL")
-	ipGeoApi := fmt.Sprintf("%s?apiKey=%s&ip=%s", apiUrl, apiKey, ipAddress)
-	fmt.Printf("ipgeo api: %s\n", ipGeoApi)
-
-	req, err := http.NewRequest(http.MethodGet, ipGeoApi, nil)
-	if err != nil {
-		fmt.Printf("ko tao dc req\n")
-		return false
-	}
-
-	response, err := client.Do(req)
-	if err != nil {
-		fmt.Printf("api call error\n")
-		return false
-	}
-
-	if response.StatusCode != 200 {
-		fmt.Printf("status code: %d\n", response.StatusCode)
-		errLogByte, _ := io.ReadAll(response.Body)
-		errLog := string(errLogByte)
-		fmt.Printf("%s\n", errLog)
-		return false
-	}
-	defer response.Body.Close()
-
-	resData, err := io.ReadAll(response.Body)
-	if err != nil {
-		fmt.Printf("ko doc duoc response\n")
-		return false
-	}
-	returnResource := jsonparse.JsonSimpleParse(resData)
-
-	// temporary
-	if _, ok := returnResource["country_code2"]; ok {
-		if returnResource["country_code2"] == "VN" {
-			return true
-		}
-	}
-
-	if _, ok := returnResource["country_code3"]; ok {
-		if returnResource["country_code3"] == "VNM" {
-			return true
-		}
-	}
-
-	return false
 }

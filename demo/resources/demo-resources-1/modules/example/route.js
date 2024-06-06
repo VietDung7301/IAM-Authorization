d69711554b62ba758428dfcab6134f85542c27cb
VietDung <vietdung.7301@gmail.com>
2024-06-07 00:44:56 +0700

    Update demo resource

--- demo/resources/demo-resources-1/modules/example/route.js
+++ demo/resources/demo-resources-1/modules/example/route.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('./controller');
+const cors = require('cors');
 
-router.get('/students', controller.getStudents);
+router.get('/students', cors({origin: [process.env.ACCESS_MODULE]}), controller.getStudents);
 
 module.exports = router;

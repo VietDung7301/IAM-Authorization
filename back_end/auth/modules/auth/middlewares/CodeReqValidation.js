49e53370cab177c8576c4091642be33d5b6c098c
Hoang Anh <hoanganhlhbk@gmail.com>
2024-06-22 09:12:26 +0700

    refractor

--- back_end/auth/modules/auth/middlewares/CodeReqValidation.js
+++ back_end/auth/modules/auth/middlewares/CodeReqValidation.js
@@ -4,9 +4,9 @@ exports.Handle = async (req, res, next) => {
     console.log('requirement validation')
     const data = req.body
 
-    if (data.response_type == null || 
-        data.client_id == null ||
-        data.redirect_uri == null)
+    if (!data.response_type || data.response_type == '' || 
+        !data.client_id || data.client_id == '' || 
+        !data.redirect_uri || data.redirect_uri == '')
         return responseTrait.ResponseInvalid(res)
 
     if (data.response_type != 'code')
